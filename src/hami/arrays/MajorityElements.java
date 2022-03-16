0c62f4f01da05c0c7d228b0103dcbda09213ab11
Afsar Khan <afsar0820@gmail.com>
2022-03-16 03:07:53 -0500

    Moore voting algo for finding majority elements in an array

--- src/hami/arrays/MajorityElements.java
+++ src/hami/arrays/MajorityElements.java
@@ -8,10 +8,55 @@ public class MajorityElements {
 
     //Given an array of size n, find if an element is present more than n/2 times.
     public static void main(String[] args) {
-        System.out.println(bruteForce(new int[]{5, 1, 4, 1, 1}));   //O(n^2)
-        System.out.println(sortedArray(new int[]{5, 1, 4, 1, 1}));   //O(nLogN)
-        System.out.println(usingSpace(new int[]{5, 1, 4, 1, 1}));   //O(n)
+        System.out.println("Max count is:"+ bruteForce(new int[]{5, 1, 4, 1, 1}));   //O(n^2)
+        System.out.println(":::::::::::::::");
+        System.out.println("Max count is:"+ sortedArray(new int[]{5, 1, 4, 1, 1}));   //O(nLogN)
+        System.out.println(":::::::::::::::");
+        System.out.println("Max count is:"+ usingSpace(new int[]{5, 1, 4, 1, 1}));   //O(n)  Space - O(n)
+        System.out.println(":::::::::::::::");
+        System.out.println("Max count is:"+ mooreVoting(new int[]{5, 1, 4, 1, 1}));   //O(n) Space - O(1) constant
+        System.out.println(":::::::::::::::");
+        System.out.println("Max count is:"+ mooreVoting(new int[]{ 1, 1, 2, 1, 3, 5, 1 }));   //O(n) Space - O(1) constant
     }
+
+    static int mooreVoting(int a[]){
+
+        if(a.length == 0) return -1;
+
+        int ansIndex=0;
+        int count=1;
+        int element=0;
+
+        for(int i=1; i<a.length; i++){  //5, 1, 4, 1, 1
+            if(a[i] == a[ansIndex]){
+                count++;
+            }else {
+                count --;
+            }
+
+            if(count ==0){
+                ansIndex = i;
+                count = 1;
+            }
+        }
+
+        //check once again if ansIndex is majority element
+        int ansCount=0;
+        for(int i=0; i<a.length; i++){
+            if(a[i] == a[ansIndex])
+                ansCount++;
+        }
+
+        if(ansCount > a.length/2){
+            element = a[ansIndex];
+            System.out.println("Element is ::"+element);
+            return ansCount;
+        }
+
+
+        return -1;  //not found
+    }
+
 
     static int usingSpace(int a[]){
         Map<Integer, Integer> map = new HashMap();
@@ -46,6 +91,7 @@ public class MajorityElements {
         int maxCount=0;
         int temp=0;
         int count =1;
+        int element=0;
         for(int i=0; i< a.length; i++){ // O(n) 1, 1, 1, 4, 5
             if(temp==0)
                 temp = a[i];
@@ -53,8 +99,9 @@ public class MajorityElements {
                 if(temp == a[i])
                     count++;
                 else {
-                    if(count > maxCount)
+                    if(count > maxCount){
                         maxCount = count;
+                    }
 
                     temp = a[i];
                     count=1;
---
+++ src/hami/recursion/LuckyNumbers.java
@@ -0,0 +1,2 @@
+package hami.recursion;public class LuckyNumbers {
+}
